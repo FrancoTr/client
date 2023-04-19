e3e3758b2853a6fce9e017bcfbcfac56e5ef93ed
Franco Traverso <franco-t20@outlook.com>
2023-04-18 23:30:05 -0300

    Add title and show predictions button

--- src/scenes/predictions/index.tsx
+++ src/scenes/predictions/index.tsx
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
-import { useTheme } from "@mui/material";
+import { useTheme, Box, Typography, Button } from "@mui/material";
 import { useGetKpisQuery } from "@/state/api";
+import DashboardBox from "@/components/DashboardBox";
+import FlexBetween from "@/components/FlexBetween";
+
 type Props = {};
 
 const Predictions = (props: Props) => {
@@ -8,7 +11,28 @@ const Predictions = (props: Props) => {
   const [isPredictions, setIsPredictions] = useState(false);
   const { data: lpiData } = useGetKpisQuery();
 
-  return <div>Predictions</div>;
+  return (
+    <DashboardBox width='100%' height='100%' p='1rem' overflow='hidden'>
+      <FlexBetween m='1rem 2.5rem' gap='0.3rem'>
+        <Box>
+          <Typography variant='h3'>Revenue and Predictions</Typography>
+          <Typography variant='h6'>
+            Charted revenue and predicted revenue based on a simple linear regression model
+          </Typography>
+        </Box>
+        <Button
+          onClick={() => setIsPredictions(!isPredictions)}
+          sx={{
+            color: palette.grey[900],
+            backgroundColor: palette.grey[700],
+            boxShadow: "0.1rem 0.1rem 0.1rem rgba(0,0,0,.4)",
+          }}
+        >
+          Show Predicted Revenue for Next Year
+        </Button>
+      </FlexBetween>
+    </DashboardBox>
+  );
 };
 
 export default Predictions;
