e29293eaa126619f1632f24a4755fc455bb6ecc5
Franco Traverso <franco-t20@outlook.com>
2023-04-19 20:00:40 -0300

    Add graph chart to predictions page

--- src/scenes/predictions/index.tsx
+++ src/scenes/predictions/index.tsx
@@ -3,17 +3,28 @@ import { useTheme, Box, Typography, Button } from "@mui/material";
 import { useGetKpisQuery } from "@/state/api";
 import DashboardBox from "@/components/DashboardBox";
 import FlexBetween from "@/components/FlexBetween";
+import {
+  CartesianGrid,
+  Label,
+  Legend,
+  Line,
+  LineChart,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
 
 type Props = {};
 
 const Predictions = (props: Props) => {
   const { palette } = useTheme();
   const [isPredictions, setIsPredictions] = useState(false);
-  const { data: lpiData } = useGetKpisQuery();
+  const { data: kpiData } = useGetKpisQuery();
 
   return (
     <DashboardBox width='100%' height='100%' p='1rem' overflow='hidden'>
-      <FlexBetween m='1rem 2.5rem' gap='0.3rem'>
+      <FlexBetween m='1rem 2.5rem' gap='1rem'>
         <Box>
           <Typography variant='h3'>Revenue and Predictions</Typography>
           <Typography variant='h6'>
@@ -31,6 +42,41 @@ const Predictions = (props: Props) => {
           Show Predicted Revenue for Next Year
         </Button>
       </FlexBetween>
+      <ResponsiveContainer width='100%' height='100%'>
+        <LineChart
+          data={formattedData}
+          margin={{
+            top: 20,
+            right: 75,
+            left: 20,
+            bottom: 80,
+          }}
+        >
+          <CartesianGrid strokeDasharray='3 3' stroke={palette.grey[800]} />
+          <XAxis dataKey='name' tickLine={false} style={{ fontSize: "10px" }}>
+            <Label value='Month' offset={-5} position='insideBottom' />
+          </XAxis>
+          <YAxis
+            domain={[12000, 26000]}
+            tickLine={false}
+            axisLine={{ strokeWidth: "0" }}
+            style={{ fontSize: "10px" }}
+            tickFormatter={(v) => `$${v}`}
+          >
+            <Label value='Revenue in USD' angle={-90} offset={-5} position='insideLeft' />
+          </YAxis>
+          <Tooltip />
+          <Legend verticalAlign='top' />
+          <Line
+            type='monotone'
+            dataKey='Actual Revenue'
+            stroke={palette.primary.main}
+            strokeWidth={0}
+            dot={{ strokeWidth: 5 }}
+          />
+          <Line type='monotone' dataKey='Regression Line' stroke='#8884d8' dot={false} />
+        </LineChart>
+      </ResponsiveContainer>
     </DashboardBox>
   );
 };
